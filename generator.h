bc30409872263cd204a292155a657861556139b6
LufyCZ <me@lufy.cz>
2023-11-02 22:54:52 +0000

    feat: generator prep

---
+++ generator.h
@@ -0,0 +1,18 @@
+/* ******************************* generator.h ****************************** */
+// // /*  Zdroje:                                                                   */
+// // /*  - IAL, studijní opora, Prof. Ing. Jan M. Honzík, Csc., Verze 14 N         */
+/* ************************************************************************** */
+/*                                                                            */
+/*  Vytvořil a zpracoval: Lukáš Witpeerd, listopad    2023                    */
+/*
+ **/
+
+#ifndef _GENERATOR_H_
+#define _GENERATOR_H_
+
+#include <stdio.h>
+
+typedef struct Generator {
+} Generator;
+
+#endif
