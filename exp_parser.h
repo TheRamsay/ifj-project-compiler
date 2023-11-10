370e0879364881e2e5e0998203a4da6ff2bfcc68
Padias <matyas.krejza@seznam.cz>
2023-11-10 12:18:43 +0100

    WIP: expresion parser

---
+++ exp_parser.h
@@ -0,0 +1,8 @@
+//
+// Created by padak on 10/26/23.
+//
+
+#ifndef IFJ_PROJECT_EXP_PARSER_H
+#define IFJ_PROJECT_EXP_PARSER_H
+
+#endif //IFJ_PROJECT_EXP_PARSER_H
---
+++ tests/exp_parser.c
@@ -0,0 +1,15 @@
+//
+// Created by padak on 10/27/23.
+//
+
+extern "C"
+{
+#include "../exp_parser.h"
+}
+
+#include "gtest/gtest.h"
+
+TEST(ScannerTest, Basic)
+{
+assert(1);
+}
