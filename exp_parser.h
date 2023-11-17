1f3c65c8397e5f4f88572c1db6a2752cd3400125
Padias <matyas.krejza@seznam.cz>
2023-11-17 20:32:52 +0100

    WIP: improoved exp parser

--- exp_parser.h
+++ exp_parser.h
@@ -6,8 +6,16 @@
 #define _EXP_PARSER_H
 
 #include "stack.h"
+#include "scanner.h"
 
-int ParseExpression(char *expressionToParse);
+const char operators[] = {TOKEN_PLUS, TOKEN_IDENTIFIER, TOKEN_STACK_BOTTOM, 'E'};
+
+typedef struct {
+    TokenType *rule;
+    TokenType result;
+} ExtendedToken;
+
+int parse_expresion(char *expressionToParse);
 
 char *charArrayFromStack(Stack *stack);
 
--- tests/exp_parser.cpp
+++ tests/exp_parser.cpp
@@ -7,10 +7,66 @@ extern "C"{
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
-TEST(ExpParserTest, Test1) {
-    char *exp = "i+i$";
 
-    int result = ParseExpression(exp);
+//Basic tests
+TEST(ExpParserTest, BasicTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i$"));
 
-    EXPECT_EQ(1, result);
+}
+
+//Add tests
+TEST(ExpParserTest, AddTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i+i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i+$"));
+}
+
+//Minus tests
+TEST(ExpParserTest, MinusTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i-i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i-$"));
+}
+
+//Equals tests
+TEST(ExpParserTest, EqualsTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i=i$"));
+    ASSERT_TRUE(ParseExpression("i+i=i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i=$"));
+}
+
+//Lower tests
+TEST(ExpParserTest, LowerTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i<i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i<$"));
+}
+
+//Lower tests
+TEST(ExpParserTest, LowerOrEqualsTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i=<i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i=<$"));
+}
+
+
+//Higher tests
+TEST(ExpParserTest, HigherTests) {
+    //Should return true
+    ASSERT_TRUE(ParseExpression("i>i$"));
+
+    //Should return false
+    ASSERT_FALSE(ParseExpression("i>$"));
 }
