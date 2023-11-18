8df6be6349cb2c2c255985c3ed3634e2584b23a8
Padias <matyas.krejza@seznam.cz>
2023-11-18 14:46:31 +0100

    WIP: ramsay's teribble advice

--- exp_parser.c
+++ exp_parser.c
@@ -7,13 +7,6 @@
 
 #define TABLE_SIZE 4
 
-enum {
-  L,   // < /SHIFT
-  R,   // > /REDUCTION
-  E,   // = /REDUCTION PREPARE
-  Err, // Invalid
-};
-
 const int precedence_table[TABLE_SIZE][TABLE_SIZE] = {
     //+ i  $  E
     {R, L, R, E},     //+
@@ -22,119 +15,118 @@ const int precedence_table[TABLE_SIZE][TABLE_SIZE] = {
     {E, Err, R, Err}  // E
 };
 
-ExtendedToken rules[] = {
-    {TOKEN_EXPRESSION_RULE, "E+E", 'E'}, {TOKEN_EXPRESSION_RULE, "i", 'E'},
-};
+Rule_t rules[] = {{TOKEN_EXPRESSION, {TOKEN_EXPRESSION, TOKEN_PLUS, TOKEN_EXPRESSION}}, {TOKEN_EXPRESSION, {TOKEN_INTEGER_LITERAL}}};
 
-int get_operator_index(char op) {
-  for (int i = 0; i < TABLE_SIZE; ++i) {
-    if (operators[i] == op) {
-      return i;
-    }
+int get_operator_index(TokenType op) {
+  switch (op) {
+  case TOKEN_PLUS:
+    return 0;
+  case TOKEN_IDENTIFIER:
+  case TOKEN_INTEGER_LITERAL:
+  case TOKEN_DECIMAL_LITERAL:
+    return 1;
+  case TOKEN_STACK_BOTTOM:
+    return 2;
+  case TOKEN_EXPRESSION:
+    return 3;
+
+  default:
+    return -1; // Operator not found
   }
-  return -1; // Operator not found
 }
 
-int get_precedence(char stack_top, char input) {
-  int stack_index = get_operator_index(stack_top);
-  int input_index = get_operator_index(input);
+Stack_token_t get_precedence(Stack_token_t stack_top, Stack_token_t input) {
+  int stack_index = get_operator_index(stack_top.token);
+  int input_index = get_operator_index(input.token);
 
   if (stack_index == -1 || input_index == -1) {
-    return Err; // Handle invalid operators
+    return (Stack_token_t){.precedence = Err}; // Handle invalid operators
   }
 
-  return precedence_table[stack_index][input_index];
+  return (Stack_token_t){.precedence = precedence_table[stack_index][input_index]};
 }
 
-char evaluate_rule(char *rightSide) {
-
+Stack_token_t evaluate_rule(Stack_token_t *tokens) {
+  TokenType *rightSide = extract_tokens_from_stack(tokens, sizeof(tokens) / sizeof(tokens[0]));
   // Check each rule
   for (int i = 0; i < sizeof(rules) / sizeof(rules[0]); ++i) {
     if (strcmp(rightSide, rules[i].rule) == 0) {
-      return rules[i].result;
+      return (Stack_token_t){.token = rules[i].result, .precedence = None};
     }
   }
 
-  return ' '; // No matching rule
+  return (Stack_token_t){.token = TOKEN_EOF, .precedence = None};
 }
 
-char get_next_char(char *str, int index) {
-  if (index >= 0 && index <= strlen(str)) {
+Stack_token_t get_next_token_temp(TokenType array[], int index) {
+  if (index >= 0 && index <= strlen(array)) {
     // Return the character at the specified index
-    return str[index];
+    return (Stack_token_t){.precedence = None, .token = array[index]};
   }
-  return '\0';
+  return (Stack_token_t){.token = TOKEN_STACK_BOTTOM, .precedence = None};
 }
 
-int parse_expresion(char *expressionToParse) {
+int parse_expresion(TokenType *expressionToParse) {
 
   int expIndex = 0;
 
-  Stack *stack = (Stack *)malloc(sizeof(Stack));
-  stack_init(stack, 100);
-
-  stack_push(stack, (int)'$');
+  void_stack_t *stack = stack_new(8192);
+  stack_push(stack, (void *)&(Stack_token_t){.token = TOKEN_STACK_BOTTOM, .precedence = None});
 
   // TODO
   // token = next_token();
-  char token = get_next_char(expressionToParse, expIndex);
+  Stack_token_t token = get_next_token_temp(expressionToParse, expIndex);
   expIndex++;
-  if (token == '\0') {
+  if (token.token == TOKEN_STACK_BOTTOM) {
     return 0;
   }
 
   while (420 == 420) {
-    char stackTop = (char)stack_top(stack);
+    Stack_token_t stackTop = *(Stack_token_t *)stack_top(stack);
 
-    int action = get_precedence(stackTop, token);
-    if (action == Err) {
+    Stack_token_t action = get_precedence(stackTop, token);
+    if (action.precedence == Err) {
       return 0;
     }
 
-    switch (action) {
+    switch (action.precedence) {
     case R:
       // Get value to be reduced
-      char *result = charArrayFromStack(stack);
+      Stack_token_t *result = arrayFromStack(stack);
 
-      char ruleProduct = evaluate_rule(result);
+      Stack_token_t ruleProduct = evaluate_rule(result);
 
-      if (ruleProduct == ' ') {
-        if (*result == 'E') {
+      if (ruleProduct.token == TOKEN_EOF) {
+        if (result[0].token == TOKEN_EXPRESSION) {
           return 1;
         }
         return 0;
       }
 
-      stackTop = (char)stack_top(stack);
+      stackTop = *(Stack_token_t *)stack_top(stack);
       action = get_precedence(stackTop, token);
-      stack_push(stack, action);
-      stack_push(stack, ruleProduct);
+      stack_push(stack, &action);
+      stack_push(stack, &ruleProduct);
 
-      // Maybe
-      // token = get_next_char(expressionToParse, expIndex);
-      // expIndex++;
-      // if (token == '\0') {
-      //   return 0;
-      // }
       break;
     case E:
-      stack_push(stack, token);
-      token = get_next_char(expressionToParse, expIndex);
+      stack_push(stack, &token);
+      token = get_next_token_temp(expressionToParse, expIndex);
       expIndex++;
-      if (token == '\0') {
+      if (token.token == TOKEN_STACK_BOTTOM) {
         return 0;
       }
       break;
 
     case L:
-      stack_push(stack, action);
-      stack_push(stack, token);
-      token = get_next_char(expressionToParse, expIndex);
+      stack_push(stack, &action);
+      stack_push(stack, &token);
+      token = get_next_token_temp(expressionToParse, expIndex);
       expIndex++;
 
-      if (token == '\0') {
-        return 0;
-      }
+      // if (token == TOKEN_STACK_BOTTOM) {
+      //   return 0;
+      // }
     }
   }
 
@@ -143,14 +135,15 @@ int parse_expresion(char *expressionToParse) {
   return 1;
 }
 
-char *charArrayFromStack(Stack *stack) {
+// Runs thought the stack until it finds an L precedence, returns the array of tokens to be reduced
+Stack_token_t *arrayFromStack(void_stack_t *stack) {
   // Check if the stack is empty
-  if (stack->topIndex == -1) {
+  if (stack->top_index == -1) {
     return NULL;
   }
 
   // Allocate memory for the array
-  char *resultArray = (char *)malloc((stack->topIndex + 1) * sizeof(char)); // +1 for the null terminator
+  Stack_token_t *resultArray = (Stack_token_t *)malloc((stack->top_index + 1) * sizeof(Stack_token_t)); // +1 for the null terminator
 
   if (resultArray == NULL) {
     fprintf(stderr, "Memory allocation failed\n");
@@ -158,11 +151,23 @@ char *charArrayFromStack(Stack *stack) {
   }
 
   int currentIndex = 0;
-  while (stack_top(stack) != L && stack->topIndex >= 0) {
-    resultArray[currentIndex++] = stack_pop(stack);
+  while ((*(Stack_token_t *)stack_top(stack)).precedence != L && stack->top_index >= 0) {
+    resultArray[currentIndex++] = *(Stack_token_t *)stack_pop(stack);
   }
   // Get rid of the L
   stack_pop(stack);
 
   return resultArray;
+}
+
+TokenType *extract_tokens_from_stack(Stack_token_t *stack_tokens, size_t size) {
+  TokenType *result = malloc(size * sizeof(TokenType));
+
+  if (result != NULL) {
+    for (size_t i = 0; i < size; ++i) {
+      result[i] = stack_tokens[i].token;
+    }
+  }
+
+  return result;
 }
--- tests/exp_parser.cpp
+++ tests/exp_parser.cpp
@@ -2,6 +2,7 @@ extern "C"{
 
 #include "../stack.h"
 #include "../exp_parser.h"
+#include "../scanner.h"
 }
 
 #include <gtest/gtest.h>
@@ -11,62 +12,63 @@ extern "C"{
 //Basic tests
 TEST(ExpParserTest, BasicTests) {
     //Should return true
-    ASSERT_TRUE(ParseExpression("i$"));
+    TokenType expresion[] = {TOKEN_DECIMAL_LITERAL,TOKEN_PLUS , TOKEN_DECIMAL_LITERAL, TOKEN_STACK_BOTTOM};
+    ASSERT_TRUE(parse_expresion(expresion));
 
 }
 
-//Add tests
-TEST(ExpParserTest, AddTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i+i$"));
+// //Add tests
+// TEST(ExpParserTest, AddTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i+i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i+$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i+$"));
+// }
 
-//Minus tests
-TEST(ExpParserTest, MinusTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i-i$"));
+// //Minus tests
+// TEST(ExpParserTest, MinusTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i-i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i-$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i-$"));
+// }
 
-//Equals tests
-TEST(ExpParserTest, EqualsTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i=i$"));
-    ASSERT_TRUE(ParseExpression("i+i=i$"));
+// //Equals tests
+// TEST(ExpParserTest, EqualsTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i=i$"));
+//     ASSERT_TRUE(ParseExpression("i+i=i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i=$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i=$"));
+// }
 
-//Lower tests
-TEST(ExpParserTest, LowerTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i<i$"));
+// //Lower tests
+// TEST(ExpParserTest, LowerTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i<i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i<$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i<$"));
+// }
 
-//Lower tests
-TEST(ExpParserTest, LowerOrEqualsTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i=<i$"));
+// //Lower tests
+// TEST(ExpParserTest, LowerOrEqualsTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i=<i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i=<$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i=<$"));
+// }
 
 
-//Higher tests
-TEST(ExpParserTest, HigherTests) {
-    //Should return true
-    ASSERT_TRUE(ParseExpression("i>i$"));
+// //Higher tests
+// TEST(ExpParserTest, HigherTests) {
+//     //Should return true
+//     ASSERT_TRUE(ParseExpression("i>i$"));
 
-    //Should return false
-    ASSERT_FALSE(ParseExpression("i>$"));
-}
+//     //Should return false
+//     ASSERT_FALSE(ParseExpression("i>$"));
+// }
