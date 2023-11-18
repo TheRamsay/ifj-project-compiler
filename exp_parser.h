24b52deb48442c93ea81b02ee1b86d5b28a27253
Padias <matyas.krejza@seznam.cz>
2023-11-18 14:46:31 +0100

    WIP: ramsay's teribble advice

--- exp_parser.h
+++ exp_parser.h
@@ -5,18 +5,33 @@
 #ifndef _EXP_PARSER_H
 #define _EXP_PARSER_H
 
-#include "stack.h"
 #include "scanner.h"
+#include "stack.h"
 
-const char operators[] = {TOKEN_PLUS, TOKEN_IDENTIFIER, TOKEN_STACK_BOTTOM, 'E'};
+const char operators[] = {TOKEN_PLUS, TOKEN_IDENTIFIER, TOKEN_STACK_BOTTOM, TOKEN_EXPRESSION};
+
+typedef enum {
+  L,    // < /SHIFT
+  R,    // > /REDUCTION
+  E,    // = /REDUCTION PREPARE
+  Err,  // Invalid
+  None, // Useless but send thank you letters to ramsay
+} Precedence;
 
 typedef struct {
-    TokenType *rule;
-    TokenType result;
-} ExtendedToken;
+  TokenType result;
+  TokenType *rule;
+} Rule_t;
 
-int parse_expresion(char *expressionToParse);
+typedef struct {
+  Precedence precedence;
+  TokenType token;
+} Stack_token_t;
 
-char *charArrayFromStack(Stack *stack);
+int parse_expresion(TokenType expressionToParse[]);
+
+TokenType *extract_tokens_from_stack(Stack_token_t *stack_tokens, size_t size);
+
+Stack_token_t *arrayFromStack(void_stack_t *stack);
 
 #endif // _EXP_PARSER_H
