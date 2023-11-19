2263c02f3283c01593d318fd399345e4cdcbdc7e
Spustnik <125836515+Spustnik@users.noreply.github.com>
2023-11-19 01:15:24 +0100

    viac totych znakov za sebou (#14)
    * fsdsafkl
    
    * najlepsi scannern
    
    * mrdky
    
    * scacnerj upu
    
    * hovno
    
    * jeba scanner jupi
    
    * jupi sipka
    
    * removed main from scanner.c
    
    * fixed tests
    
    ---------
    
    Co-authored-by: TheRamsay <ramsayofbolton@gmail.com>

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include "scanner.h"
 #include "stdio.h"
 
-int main() { Scan(stdin); }
+int main() {
+  FILE *input_file = stdin;
+  Token token;
+  scanner_init(input_file);
+  while (get_next_token(&token) != TOKEN_EOF)
+    ;
+
+  scanner_destroy();
+
+  return 0;
+}
--- scanner.h
+++ scanner.h
@@ -1,3 +1,104 @@
-#include "stdio.h"
+#ifndef __SCANNER_H__
+#define __SCANNER_H__
 
-int Scan(FILE *file);
+#include <stdio.h>
+
+typedef enum
+{
+  NONE,
+  KW_IF,
+  KW_ELSE,
+  KW_WHILE,
+  KW_INT,
+  KW_FLOAT,
+  KW_RETURN,
+  KW_VOID,
+  KW_NIL,
+  KW_LET,
+  KW_UNKNOWN,
+  KW_VAR,
+  KW_IN,
+  KW_FOR,
+  KW_DOUBLE,
+  KW_STRING,
+  KW_FUNC,
+} Keyword;
+
+typedef enum
+{
+  TOKEN_KEYWORD,
+  TOKEN_STRING_LITERAL,
+  TOKEN_COLON,
+  TOKEN_ARROW,
+  TOKEN_IDENTIFIER,
+  TOKEN_LPAREN,
+  TOKEN_RPAREN,
+  TOKEN_LBRACE,
+  TOKEN_RBRACE,
+  TOKEN_SEMICOLON,
+  TOKEN_COMMA,
+  TOKEN_ASSIGN,
+  TOKEN_EQ,
+  TOKEN_NEQ,
+  TOKEN_LT,
+  TOKEN_LE,
+  TOKEN_GT,
+  TOKEN_GE,
+  TOKEN_NE,
+  TOKEN_PLUS,
+  TOKEN_MINUS,
+  TOKEN_MULTIPLY,
+  TOKEN_DIV,
+  TOKEN_MOD,
+  TOKEN_AND,
+  TOKEN_OR,
+  TOKEN_NOT,
+  TOKEN_LBRACKET,
+  TOKEN_RBRACKET,
+  TOKEN_PLUS_ASSIGN,
+  TOKEN_MINUS_ASSIGN,
+  TOKEN_MULTIPLY_ASSIGN,
+  TOKEN_DIV_ASSIGN,
+  TOKEN_MOD_ASSIGN,
+  TOKEN_AND_ASSIGN,
+  TOKEN_OR_ASSIGN,
+  TOKEN_NOT_ASSIGN,
+  TOKEN_XOR_ASSIGN,
+  TOKEN_LSHIFT_ASSIGN,
+  TOKEN_RSHIFT_ASSIGN,
+  TOKEN_LSHIFT,
+  TOKEN_RSHIFT,
+  TOKEN_BITWISE_AND,
+  TOKEN_BITWISE_OR,
+  TOKEN_BITWISE_XOR,
+  TOKEN_BITWISE_NOT,
+  TOKEN_NULL_COALESCING,
+  TOKEN_TERNARY,
+  TOKEN_VAR,
+  TOKEN_EOF,
+  TOKEN_UNKNOWN,
+  TOKEN_PRINT,
+  TOKEN_IN,
+  TOKEN_FOR,
+  TOKEN_OPTIONAL_TYPE,
+  TOKEN_EXPONENT,
+  TOKEN_DECIMAL_LITERAL,
+  TOKEN_INTEGER_LITERAL,
+} TokenType;
+
+typedef struct
+{
+  TokenType type;
+  Keyword keyword;
+  char *val;
+  int length;
+} Token;
+
+void scanner_init(FILE *file);
+void scanner_destroy();
+void token_destroy(Token *token);
+int get_next_token(Token *token);
+void char_to_token(Token *token, char c);
+void determine_token_type(Token *token);
+
+#endif // __SCANNER_H__
