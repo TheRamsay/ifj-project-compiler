2896ff30706a205348afc2a7b50756dafea55ac3
Spustnik <1psimonova1@gmail.com>
2023-12-05 10:36:36 +0000

    bez mainu

--- scanner.c
+++ scanner.c
@@ -564,18 +564,18 @@ int get_next_token(Token *token) {
     fprintf(stderr, "Unknown token: %s\n", token->val);
     exit(1);
   }
-  printf("Token Type: %d token: %s newline: %d\n", token->type, token->val, token->after_newline);
+  // printf("Token Type: %d token: %s newline: %d\n", token->type, token->val, token->after_newline);
   return token->type;
 }
 
-int main() {
-  FILE *input_file = stdin;
-  Token token;
-  scanner_init(input_file);
-  while (get_next_token(&token) != TOKEN_EOF)
-    ;
+// int main() {
+//   FILE *input_file = stdin;
+//   Token token;
+//   scanner_init(input_file);
+//   while (get_next_token(&token) != TOKEN_EOF)
+//     ;
 
-  scanner_destroy();
+//   scanner_destroy();
 
-  return 0;
-}
+//   return 0;
+// }
--- test.swift
+++ test.swift
@@ -1,3 +1,1 @@
-// stolen from pepega007xd
-let result = substring(of: "123456789", startingAt: 0, endingBefore: 10) 
-write(result)
+25e-2
